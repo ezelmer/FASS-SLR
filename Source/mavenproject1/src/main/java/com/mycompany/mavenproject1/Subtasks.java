ae9f811e80c854606f9d983d5a88dccf787db030
cattiestsine75 <ethan.zelmer@gmail.com>
2023-07-25 09:30:09 -0400

    Works of authors filed by SLR, as well as a master list for all authors.

--- Source/mavenproject1/src/main/java/com/mycompany/mavenproject1/Subtasks.java
+++ Source/mavenproject1/src/main/java/com/mycompany/mavenproject1/Subtasks.java
@@ -11,7 +11,9 @@ import static com.mycompany.mavenproject1.Main.getHTML;
 import static com.mycompany.mavenproject1.Main.grabTag;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Scanner;
 //problems: 24, 34, 40, 41, 54, 55, 57, 62, 68, 76, 80, 84, 87, 88, 92, 94, 99, 100, 101, 
 
 /**
@@ -24,6 +26,135 @@ public class Subtasks {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        ArrayList<SLR> slrs = initialize();
+        ArrayList<Author> mAuthors = new ArrayList<>();
+        for (int i = 2; i <= 112; i++) {
+            System.out.println("Starting work on document " + i);
+            ArrayList<Author> authors = new ArrayList<>();
+            SLR s = slrs.get(i);
+            int curloc = 0;
+            int prevloc = 0;
+            int count = 0;
+            try {
+                Scanner reader = new Scanner(new File("C:\\Users\\ethan\\Desktop\\2023USRAResearch\\FASS-SLR\\FASS-SLR\\Dataset\\Additional_Files\\citationGraph\\" + i + ".txt"));
+                String in = "";
+                while (reader.hasNextLine()) {
+                    in = in + reader.nextLine();
+                }
+                System.out.println(in);
+                if (in.length() > 10) {
+                    while (in.indexOf("\"Full Text\":\"", curloc + 1) != -1) {
+                        prevloc = curloc + 1;
+                        System.out.println("\nTEXT:  ");
+                        count++;
+                        curloc = in.indexOf("\"Full Text\":\"", prevloc);
+                        System.out.println("got curloc" + curloc);
+                        String x = in.substring(prevloc, curloc);
+                        System.out.println("Got x\n\nx:\n\n" + x);
+                        x = x.substring(x.lastIndexOf("\"authors\":["));
+                        System.out.println("filtered x");
+                        System.out.println(x);
+
+                        int authorloc = 0;
+                        while (x.indexOf("firstname\":\"", authorloc) != -1) {
+                            authorloc = x.indexOf("firstname\":\"", authorloc);
+
+                            String fn = grabTag(x.substring(authorloc), "firstname\":\"", "\"", true);
+                            String ln = grabTag(x.substring(authorloc), "surname\":\"", "\"", true);
+                            authorloc++;
+                            System.out.println(fn + " " + ln);
+                            Author a = new Author();
+
+                            a.firstname = fn;
+                            a.surname = ln;
+                            if (!authors.contains(a)) {
+                                authors.add(a);
+                            }
+                            if (!mAuthors.contains(a)) {
+                                mAuthors.add(a);
+                            }
+                        }
+
+                    }
+                }
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            //file by file writing.
+            try {
+                String out = "";
+                for (int j = 0; j < authors.size(); j++) {
+                    System.out.println("Trying to get index " + j);
+                    Author a = authors.get(j);
+                    try {
+                        Thread.sleep(300);
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    if (!a.firstname.equals("NULL") && !a.surname.equals("NULL") && !a.firstname.contains("?") && !a.surname.contains("?")) {
+                        System.out.println("(" + (j + 1) + " out of " + authors.size() + "). searching for " + a.firstname + " " + a.surname + "'s works:");
+                        String base = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=" + a.firstname.replace(" ", "+") + "+" + a.surname.replace(" ", "+") + "[Author]";
+                        String ret = getHTML(base);
+                        out = out + a.firstname + " " + a.surname + "\n";
+                        while (ret.contains("<Id>")) {
+                            out = out + grabTag(ret, "Id", false) + "\n";
+                            ret = ret.substring(ret.indexOf("<Id") + 1);
+                            //  System.out.println(ret);
+                        }
+                    }
+
+                }
+                FileWriter writer = new FileWriter(new File("C:\\Users\\ethan\\Desktop\\2023USRAResearch\\FASS-SLR\\FASS-SLR\\Dataset\\Additional_Files\\authorWorks\\" + i + ".txt"));
+
+                writer.write(out);
+                writer.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            System.out.println("count:" + count);
+        }
+        
+        
+        
+        
+        //master writing
+        try {
+            String out = "";
+            for (int j = 0; j < mAuthors.size(); j++) {
+
+                System.out.println("Trying to get index " + j);
+                Author a = mAuthors.get(j);
+                try {
+                    Thread.sleep(300);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                if (!a.firstname.equals("NULL") && !a.surname.equals("NULL") && !a.firstname.contains("?") && !a.surname.contains("?")) {
+                    System.out.println("(" + (j + 1) + " out of " + mAuthors.size() + "). searching for " + a.firstname + " " + a.surname + "'s works:");
+                    String base = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=" + a.firstname.replace(" ", "+") + "+" + a.surname.replace(" ", "+") + "[Author]";
+                    String ret = getHTML(base);
+                    out = out + a.firstname + " " + a.surname + "\n";
+                    while (ret.contains("<Id>")) {
+                        out = out + grabTag(ret, "Id", false) + "\n";
+                        ret = ret.substring(ret.indexOf("<Id") + 1);
+                        //  System.out.println(ret);
+                    }
+                }
+
+            }
+
+            FileWriter writer = new FileWriter(new File("C:\\Users\\ethan\\Desktop\\2023USRAResearch\\FASS-SLR\\FASS-SLR\\Dataset\\Additional_Files\\authorWorks\\master.txt"));
+            writer.write(out);
+            writer.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public static void fullTextAndCitations() {
+
         // TODO code application logic here
         ArrayList<SLR> slrs = initialize();
         int minFTRefs = 999;
@@ -31,12 +162,9 @@ public class Subtasks {
         int slrNum = 0;
         int curlow = -1;
         ArrayList<Integer> retrievals = new ArrayList<>();
-        
+
         //can't tell what is full text vs just abstract
-        
-     
-        
-        for (int i = 105; i <=112; i++) {
+        for (int i = 2; i <= 112; i++) {
             File f = new File("C:\\Users\\ethan\\Desktop\\2023USRAResearch\\FASS-SLR\\FASS-SLR\\Dataset\\Additional_Files\\citationGraph\\" + i + ".txt");
             try {
                 FileWriter writer = new FileWriter(f);
@@ -58,15 +186,15 @@ public class Subtasks {
                                 numRefsComplete++;
                                 //r.toJson.substring(1,length-1) + , 
                                 r.FT.replaceAll("\"", "'");
-                                
-                                output = output + "{" + r.toJson().substring(1,r.toJson().length() -1)+",\"Full Text\":\""+r.FT  + "\",\"References depth 1\":[";
+
+                                output = output + "{" + r.toJson().substring(1, r.toJson().length() - 1) + ",\"Full Text\":\"" + r.FT + "\",\"References depth 1\":[";
 
                                 for (Reference rr : r.references) {
                                     if (rr.doi.indexOf("10.") == 0) {
                                         rr.populate();
                                     }
                                     String title = rr.title;
-                                    output = output + "{" + rr.toJson().substring(1,rr.toJson().length() -1) + ",\"References depth 2\":[";
+                                    output = output + "{" + rr.toJson().substring(1, rr.toJson().length() - 1) + ",\"References depth 2\":[";
                                     System.out.println("\t" + rr.title);
                                     title = title.replace(" ", "%20");
                                     String url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=xml&retmax=1&term=" + title + "&field=title";
@@ -77,7 +205,7 @@ public class Subtasks {
                                             rrr.populate();
                                         }
                                         String x = rrr.title;
-                                        output = output + "{" + rrr.toJson().substring(1,rrr.toJson().length() -1) + "},";
+                                        output = output + "{" + rrr.toJson().substring(1, rrr.toJson().length() - 1) + "},";
                                         System.out.println("\t\t" + x);
                                     }
                                     if (output.charAt(output.length() - 1) == ',') {
@@ -109,15 +237,11 @@ public class Subtasks {
 
             //grab 3, 5 random articles, citation graph depth of 2, and citation network.
         }
-        
-        
-       /*
-        include only the ones with source_x = "PMC
-        */ 
-        
-    }
 
-   
+        /*
+        include only the ones with source_x = "PMC
+         */
+    }
 
     public static String formatAsAbstract(String Abstract) {
         for (int i = 0; i < Abstract.length(); i++) {
